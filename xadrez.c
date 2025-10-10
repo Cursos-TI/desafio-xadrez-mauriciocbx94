e526241e4111d366c1c1ac5467dcfd8841d2ad65
mauriciocbx94 <mauriciocbx1994@gmail.com>
2025-10-10 02:20:56 +0000

    desafioxadrez

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
-int main(){
-    prinft("Desafio Xadrez!\n");
-    printf("Novo commit\n");
-}
+// Constantes para definir o número de movimentos de cada peça
+int bispo = 5;
+int torre = 5;
+int rainha = 8;
 
+int main() {
+    
+    printf("Movimentação do Bispo:\n");
+    for (int i = 0; i < bispo; i++) {
+        printf("Cima\n");
+        printf("Direita\n");
+    }
+
+    printf("\nMovimentação da Torre:\n");
+    int j = 0;
+    while (j < torre) {
+        printf("Direita\n");
+        j++;
+    }
+
+    printf("\nMovimentação da Rainha:\n");
+    int k = 0;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k < rainha);
+
+    return 0;
+}
